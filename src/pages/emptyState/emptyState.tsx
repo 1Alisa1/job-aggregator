2bd01de978351f51bfb408ef62a45621ecfebbda
1Alisa1 <Alisa.dragun@mail.ru>
2023-05-16 12:32:08 +0300

    add emty state section

--- src/pages/emptyState/emptyState.tsx
+++ src/pages/emptyState/emptyState.tsx
@@ -1,5 +1,18 @@
+import styles from "./emptyState.module.scss";
+import notFoundImg from "../../img/notFoundImg.svg";
+import { useNavigate } from "react-router-dom";
 const EmptyState: React.FC = () => {
-  return (<div></div>);
+  const navigate = useNavigate();
+  
+  return (
+  <section className={styles.NotFoundSection}>
+    <div className={styles.img}>
+      <img src={notFoundImg} alt="NotFound"></img>
+    </div>
+    <div className={styles.text}>Упс, здесь еще ничего&nbsp;нет!</div>
+    <button onClick={() => navigate("/vacancies")}>Поиск вакансий</button>
+  </section>
+  );
 }
 
 export {EmptyState};
