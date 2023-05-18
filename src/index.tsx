dcbec5de38a0b8d2b278ca201e13f36b96d133f7
1Alisa1 <Alisa.dragun@mail.ru>
2023-05-18 23:33:21 +0300

    add scroll to top

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../src/scss/index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/scss/index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/scrollToTop/scrollToTop";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>
 );
