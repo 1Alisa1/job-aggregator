7cbf247d5b26fd61fdbbcd7ea80fb133ff52cf36
1Alisa1 <Alisa.dragun@mail.ru>
2023-05-19 16:53:45 +0300

    add navigation modal

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,8 +3,11 @@ import logo from "../../img/logo.svg";
 import navBtn from "../../img/navBtn.svg";
 import styles from "./layout.module.scss";
 import Navigation from "../navigation/navigation";
+import NavModal from "../navModal/navModal";
+import { useState } from "react";
 
 const Layout: React.FC = () => {
+  const [isNavModalOpen, setIsNavModalOpen] = useState(false);
   return (
     <>
       <header>
@@ -16,7 +19,13 @@ const Layout: React.FC = () => {
         <div className={styles.headerNav}>
           <Navigation />
         </div>
-        <img src={navBtn} alt="navBtn" className={styles.navBtn}></img>
+        <img src={navBtn} alt="navBtn" className={styles.navBtn} onClick={() => setIsNavModalOpen(true)}></img>
+        <NavModal active={isNavModalOpen} setActive={setIsNavModalOpen}>
+            <div className={styles.x} onClick={() => setIsNavModalOpen(false)}>
+              <span>+</span>
+            </div>
+            <Navigation setActiveModal={setIsNavModalOpen}/>
+        </NavModal>
       </header>
       <main>
         <Outlet />
